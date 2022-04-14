f09231243cf043cb2def8cae1c320faabddce34a
jmz <bowden@htw-berlin.de>
2022-04-14 23:01:23 +0200

    Add a "Hello World" page to the contacts app
    - We create a function in `contact/views.py` that returns "Hello World"
      as an `HttpResponse`
    
    - We add a path to `foo/urls.py` which makes this response appear when
      we access `http://127.0.0.1:8000/contact/` in a web browser
    
    - To test this we can run the server with the following commands:
    
      $ cd foo
      $ python manage.py runserver

--- foo/foo/urls.py
+++ foo/foo/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from contact import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contact/', views.index, name="contact"),
 ]
